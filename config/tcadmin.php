c1808201f13e563467f4122a2b9200cc9f6feeab
JamiesServers <jamie@jamies-servers.co.uk>
2015-03-21 12:58:03 +0000

    Updated to 0.1.0

---
+++ config/tcadmin.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * TCAdmin - The Game Hosting Control Panel
+ * 
+ * @author Jamie Sage
+ * @link https://www.jamies-servers.co.uk
+ *
+ * With thanks to the Blesta team and TCAdmin
+ * @link http://www.blesta.com/
+ * @link http://www.tcadmin.com/
+ * @link http://help.tcadmin.com/Billing_API_Examples
+ */
